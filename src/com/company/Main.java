52c3b6b659446d8244395c4301c33a0f24a2d7eb
ofung <luvliv918@gmail.com>
2017-05-31 21:55:12 -0400

    Disallowed special characters for firstName

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,29 @@
 package com.company;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
         Scanner nameInput = new Scanner(System.in);
-        System.out.println("Please enter your first name.");
-        String firstName = nameInput.next();
-        System.out.println(firstName);
+        Pattern namePattern = Pattern.compile("[a-zA-Z]+");
+        boolean booleanValue = true;
+        String firstName;
+        while(booleanValue)
+        {
+            System.out.println("Please enter your first name.");
+            firstName = nameInput.next();
+            Matcher nameMatcher = namePattern.matcher(firstName);
+            boolean matched = nameMatcher.matches();
+            if (matched) {
+                System.out.println(firstName);
+                booleanValue = false;
+            } else {
+                System.out.println("Invalid Character.  Please enter alphabetical characters only.");
+            }
+
+        }
 
         System.out.println("How many letters are in your last name?");
         Integer lastNameCount = nameInput.nextInt();
@@ -16,13 +32,21 @@ public class Main {
         String[] lastName = new String[lastNameCount];
 
         System.out.println("Please enter your last name, one letter at a time.  Press \"enter\" after each letter.");
-        for (int i = 0; i < lastNameCount; i++) {
+//        for (int i = 0; i < lastNameCount; i++) {
+//            String lastNameLetters = nameInput.next();
+//            lastName[i] = lastNameLetters;
+//        }
+        int i = 0;
+        while(lastNameCount > i) {
             String lastNameLetters = nameInput.next();
             lastName[i] = lastNameLetters;
+           i++;
         }
-        for (int i = 0; i < lastNameCount; i++) {
-            System.out.print(lastName[i]);
+        // Fix to only take the correct letter if user enters more than 1
+        for (int j = 0; j < lastNameCount; j++) {
+            System.out.print(lastName[j]);
         }
+
 
     }
 }
